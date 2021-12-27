4103f340b40569d9641ae6a4c94f6fa1e0b44dbc
BenTho-Uni <benjamin.thomitzni@iwr.uni-heidelberg.de>
2021-12-27 14:19:01 +0100

    added doxygen and workflow for auto publishing (hopefully)

---
+++ test.cpp
@@ -0,0 +1,32 @@
+/**
+@mainpage
+
+This is a test application.
+
+@author Test
+@date 10/21/2021
+@version 1.0
+*/
+
+/** @file */
+
+
+#include <iostream>
+
+using namespace std;
+
+/**
+* This is a brief doxygen description
+*
+* Long Description
+*/
+int test_function (int test);
+
+int test_function (int test){
+  if (test == 1) return 1;
+  else return 0;
+}
+
+int main () {
+  cout << "Test" << endl;
+}
---
+++ test.h
@@ -0,0 +1,10 @@
+
+/**
+* This is a brief class description
+*
+* This is a long class discription
+*/
+
+class test_class {
+  int member_att = 3; //!< Brief member description
+};
